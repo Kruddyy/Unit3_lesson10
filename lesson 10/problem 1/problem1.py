60cda481f21fbe2e8ecd826543c98b5091063f47
Kruddyy <43756292+Kruddyy@users.noreply.github.com>
2018-12-18 12:40:42 -0500

    Add files via upload

---
+++ lesson 10/problem 1/problem1.py
@@ -0,0 +1,11 @@
+print('-'*65)
+print('Life decison Bot:')
+print()
+raining = input('Is it raining out?')
+if raining == 'Yes':
+	print('Stay at home!')
+	print('Maybe binge watch a show on netflix?')
+else:
+	print('Go out and do something!')
+	print('Maybe go on a hike!')
+print('-'*65)
---
+++ lesson 10/problem 3/problem3.py
@@ -0,0 +1,10 @@
+print('-'*65)
+print('Bouncer bot:')
+print()
+age = input('What is your age today? ')
+age = int(age)
+if age < 21:
+	print('your gonna need to backup this club is 21+ only so find somewhere else to party or find out what robot punches feel like.')
+else:
+	print('Cool, come on in. ')
+print('-'*65)
